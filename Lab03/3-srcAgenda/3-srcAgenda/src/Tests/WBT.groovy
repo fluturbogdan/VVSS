5f01b3865f65143242c369fbe9481f23cd6c4a20
fluturbogdan <cragoliof@gmail.com>
2018-03-27 02:35:47 +0300

    Lab 3 reworked

--- Lab03/3-srcAgenda/3-srcAgenda/src/Tests/WBT.groovy
+++ Lab03/3-srcAgenda/3-srcAgenda/src/Tests/WBT.groovy
@@ -1,5 +1,6 @@
 package Tests
 
+import model.base.Contact
 import repository.classes.RepositoryContactFile
 
 /**
@@ -7,18 +8,25 @@ import repository.classes.RepositoryContactFile
  */
 class WBT extends GroovyTestCase {
     void testWB1() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        if(repc.saveContracts("bin\\files\\contacts.txt")==true)
-            assert true;
+        RepositoryContactFile repc=new RepositoryContactFile()
+        if(repc.saveContracts("bin\\files\\contacts.txt")) {
+            repc.clear()
+            repc.addContact(new Contact("nume1","adresa1","0712312312","email1@email.com"))
+            repc.getContacts()
+            if (repc.count()!=1) assert false
+            repc.removeContact(new Contact("nume1","adresa1","0712312312","email1@email.com"))
+            if(repc.count()>0) assert false
+            assert true
+        }
         else
-            assert false;
+            assert false
     }
 
     void testWB2() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        if(repc.saveContracts("bin\\asd\\contacts.txt")==false)
-            assert true;
+        RepositoryContactFile repc=new RepositoryContactFile()
+        if(!repc.saveContracts("bin\\asd\\contacts.txt"))
+            assert true
         else
-            assert false;
+            assert false
     }
 }
