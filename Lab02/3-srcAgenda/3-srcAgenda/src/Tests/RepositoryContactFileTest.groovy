3826358b74ec79a5909d49321662c2f84ca70451
fluturbogdan <cragoliof@gmail.com>
2018-03-26 17:02:51 +0300

    update

--- Lab02/3-srcAgenda/3-srcAgenda/src/Tests/RepositoryContactFileTest.groovy
+++ Lab02/3-srcAgenda/3-srcAgenda/src/Tests/RepositoryContactFileTest.groovy
@@ -9,60 +9,60 @@ import repository.classes.RepositoryContactFile
  */
 class RepositoryContactFileTest extends GroovyTestCase {
     void testTCEP1() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
             repc.addContact(new Contact("nume1","adresa1","0712312312","email1@email.com"))
             repc.addContact(new Contact("nume2","adresa2","0712312313","email2@email.com"))
 
-            if(repc.count()!=2) assert false;
+            if(repc.count()!=2) assert false
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
 
         }
     }
 
     void testTCEP2() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
             repc.addContact(new Contact("123.23","12412","asdasdasd","asd@asd@asd.asd"))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
         }
     }
 
     void testTCEP3() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
             repc.addContact(new Contact("","asd","09876543456","asd@asd@asd.asd"))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
         }
     }
 
     void testTCEP4() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
             repc.addContact(new Contact("nume2","adresa2","",""))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
         }
     }
 
     void testTCBVA3() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("M","ceva adresa","0123123123","ceva@ceva.ceva"));
-            if(repc.count()!=1) assert false;
+            repc.addContact(new Contact("M","ceva adresa","0123123123","ceva@ceva.ceva"))
+            if(repc.count()!=1) assert false
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
         }
     }
 
@@ -110,26 +110,26 @@ class RepositoryContactFileTest extends GroovyTestCase {
 //    }
 
     void testTCBVA7() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","","0123123123","ceva@ceva.ceva"));
+            repc.addContact(new Contact("Nume","","0123123123","ceva@ceva.ceva"))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
 
         }
     }
 
     void testTCBVA9() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","D","0123123123","ceva@ceva.ceva"));
-            if(repc.count()!=1) assert false;
+            repc.addContact(new Contact("Nume","D","0123123123","ceva@ceva.ceva"))
+            if(repc.count()!=1) assert false
 
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
 
         }
     }
@@ -178,81 +178,81 @@ class RepositoryContactFileTest extends GroovyTestCase {
 //    }
 
     void testTCBVA14() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","0000000000","ceva@ceva.ceva"));
-            if(repc.count()!=1) assert false;
+            repc.addContact(new Contact("Nume","ceva adresa","0000000000","ceva@ceva.ceva"))
+            if(repc.count()!=1) assert false
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
 
         }
     }
 
     void testTCBVA15() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","999999999","ceva@ceva.ceva"));
+            repc.addContact(new Contact("Nume","ceva adresa","999999999","ceva@ceva.ceva"))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
         }
     }
 
     void testTCBVA16() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","0000000001","ceva@ceva.ceva"));
-            if(repc.count()!=1) assert false;
+            repc.addContact(new Contact("Nume","ceva adresa","0000000001","ceva@ceva.ceva"))
+            if(repc.count()!=1) assert false
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
 
         }
     }
 
     void testTCBVA17() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","0999999998","ceva@ceva.ceva"));
-            if(repc.count()!=1) assert false;
+            repc.addContact(new Contact("Nume","ceva adresa","0999999998","ceva@ceva.ceva"))
+            if(repc.count()!=1) assert false
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
         }
     }
 
     void testTCBVA18() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","1000000000","ceva@ceva.ceva"));
+            repc.addContact(new Contact("Nume","ceva adresa","1000000000","ceva@ceva.ceva"))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
         }
     }
 
     void testTCBVA19() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","0999999999","ceva@ceva.ceva"));
-            if(repc.count()!=1) assert false;
+            repc.addContact(new Contact("Nume","ceva adresa","0999999999","ceva@ceva.ceva"))
+            if(repc.count()!=1) assert false
         }
-        catch (InvalidFormatException e){
+        catch (InvalidFormatException ignored){
         }
     }
 
     void testTCBVA22() {
-        RepositoryContactFile repc=new RepositoryContactFile();
-        repc.clear();
+        RepositoryContactFile repc=new RepositoryContactFile()
+        repc.clear()
         try{
-            repc.addContact(new Contact("Nume","ceva adresa","0999999999","E"));
+            repc.addContact(new Contact("Nume","ceva adresa","0999999999","E"))
         }
-        catch (InvalidFormatException e){
-            if(repc.count()!=0) assert false;
+        catch (InvalidFormatException ignored){
+            if(repc.count()!=0) assert false
         }
     }
 
